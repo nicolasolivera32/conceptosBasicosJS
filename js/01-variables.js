869e61be096fef7286350335e8a27c74dca289a9
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-10-26 22:56:19 -0300

    usamos ventana emergente con alert, luego solicitamos un numero para guardarlo en una variable y le sumamos 5 y se mostro el resultado por console.log

--- js/01-variables.js
+++ js/01-variables.js
@@ -15,3 +15,21 @@ console.log("año actual: " + anioActual);
 anioActual = 2023 + 1;
 console.log("año actual: " + anioActual);
 console.log(url);
+
+document.write("hola mundo");
+document.write("<h1 class='titulo'>hola mundo " + anioActual + "</h1>");
+
+// ventana emergente
+
+alert("este es un ejemplo de alerta");
+
+// solicitarle un numero al usuario y luego sumarle 5, por ultimo mostrar el resultado de la suma.
+
+const numeroSolicitado = parseInt(prompt("ingrese un numero del 1 al 100"));
+console.log(numeroSolicitado);
+console.log("resultado: " + (numeroSolicitado + 5));
+console.log(numeroSolicitado + 5)
+
+// transformar o parsear un numero de string a formato numerico
+// parseInt ("100") enteros
+// parseFloat("100.5") decimales
