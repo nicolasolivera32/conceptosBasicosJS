3252cccf077ae0dc1da9d6f31fa96b4cfef78c4a
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-10-26 21:23:14 -0300

    declaramos e inicializamos una variable con lent y const

---
+++ js/01-variables.js
@@ -0,0 +1,17 @@
+// declarar una variable (var - let - const)
+
+let anioActual = 2023;
+const url =
+  "https://universe.rollingcodeschool.com/live/videos/64a888547f8ffc05a8b8f37c";
+
+// mostrar mensajes
+
+console.log(anioActual);
+console.log("anioActual");
+console.log("año actual: " + anioActual);
+
+// modificar el valor de una variable
+
+anioActual = 2023 + 1;
+console.log("año actual: " + anioActual);
+console.log(url);
