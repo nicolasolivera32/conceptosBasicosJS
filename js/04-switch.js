e33c1c7ebdc652a58338383a263275f9ec5d2467
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-11-02 19:51:47 -0300

    agregamos un bucle do-while al ejercicio del cajero

--- js/04-switch.js
+++ js/04-switch.js
@@ -1,11 +1,5 @@
 // si el usuario presiona 1 consulta el saldo, 2 extraer dinero, 3 ingresar dinero
 
-let saldo = 1000;
-
-const opcion = prompt(
-  "Seleccione una opción: 1-consultar saldo, 2- extraer dinero, 3- ingresar dinero"
-);
-
 // if (opcion === 1) {
 //   alert(`su saldo disponible es: $${saldo}`);
 // } else if (opcion === 2) {
@@ -36,36 +30,46 @@ const opcion = prompt(
 
 // Ejercicio resuelto con switch
 
-switch (opcion) {
-  case "saldo":
-  case "consultar saldo":
-  case "1":
-    alert(`su saldo disponible es: $${saldo}`);
-    break;
-  case 2:
-    const monto = parseFloat(prompt("Ingrese la cantidad que desea extraer"));
-    if (monto > 0 && monto <= saldo) {
-      alert(
-        `Su extracción fue exitosa. Su saldo actual es de: $${saldo - monto}`
+let saldo = 1000;
+
+do{
+
+  const opcion = prompt(
+    "Seleccione una opción: 1-consultar saldo, 2- extraer dinero, 3- ingresar dinero"
+  );
+    
+  switch (opcion) {
+    case "saldo":
+    case "consultar saldo":
+    case "1":
+      alert(`su saldo disponible es: $${saldo}`);
+      break;
+    case "2":
+      const monto = parseFloat(prompt("Ingrese la cantidad que desea extraer"));
+      if (monto > 0 && monto <= saldo) {
+        alert(
+          `Su extracción fue exitosa. Su saldo actual es de: $${saldo - monto}`
+        );
+      } else {
+        alert("Fondos insuficientes para realizar el retiro");
+      }
+      break;
+    case "3":
+      const montoIngresar = parseFloat(
+        prompt("Ingrese la cantidad que desea depositar en su cuenta:")
       );
-    } else {
-      alert("Fondos insuficientes para realizar el retiro");
-    }
-    break;
-  case 3:
-    const montoIngresar = parseFloat(
-      prompt("Ingrese la cantidad que desea depositar en su cuenta:")
-    );
-    if (montoIngresar > 0) {
-      alert(
-        `Depósito realizado con éxito. Su saldo actual es de: $${
-          montoIngresar + saldo
-        }`
-      );
-    } else {
-      alert(`Ingrese un monto mayor a 0 (cero)`);
-    }
-    break;
-  default:
-    alert("Ingresó una opción inválida");
-}
+      if (montoIngresar > 0) {
+        alert(
+          `Depósito realizado con éxito. Su saldo actual es de: $${
+            montoIngresar + saldo
+          }`
+        );
+      } else {
+        alert(`Ingrese un monto mayor a 0 (cero)`);
+      }
+      break;
+    default:
+      alert("Ingresó una opción inválida");
+  }
+}while(confirm("¿Desea realizar otra operación?"))
+
